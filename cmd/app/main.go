16d31b85c3535517a31ba8d9f6b9d92130928dd7
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-03-19 16:16:02 +0300

    Add logging

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/joho/godotenv"
@@ -15,15 +16,28 @@ type File struct {
 	executable  bool
 }
 
+type Logger struct {
+	info  *log.Logger
+	error *log.Logger
+}
+
 func main() {
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	logger := Logger{
+		info:  infoLog,
+		error: errorLog,
+	}
+
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("Could not load .env file - relying on flags and defaults...")
+		logger.info.Println("Could not load .env file - relying on flags and defaults...")
 	}
 
 	cfg, err := loadConfig()
 	if err != nil {
-		log.Fatalf("Error loading config: %v", err)
+		logger.error.Fatalf("Error loading config: %v", err)
 	}
 
 	xrayExecutable := File{
