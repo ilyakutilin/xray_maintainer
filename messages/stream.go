be3909c73e0d6341b8630a19a5ad1606bbbe44d3
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-05-06 16:17:52 +0300

    Add stream sender

---
+++ messages/stream.go
@@ -0,0 +1,36 @@
+package messages
+
+import "fmt"
+
+// StreamSender implements Sender for debugging (to stdout)
+type StreamSender struct{}
+
+// Send sends a message using the StreamSender. It prints the message details,
+// including the subject, body, and any associated errors, to the standard output.
+// If there are no errors, it indicates so. If there is one error, it prints the error.
+// If there are multiple errors, it lists them all.
+//
+// Parameters:
+//   - msg: The Message to be sent, containing a subject, body, and a list of errors.
+//
+// Returns:
+//   - error: Always returns nil as this implementation does not perform actual sending.
+func (e *StreamSender) Send(msg Message) error {
+	fmt.Println("The following message would be sent:")
+	fmt.Printf("Subject: %s\n", msg.Subject)
+	fmt.Printf("Body: %s\n", msg.Body)
+
+	errorsCount := len(msg.Errors)
+	switch errorsCount {
+	case 0:
+		fmt.Println("There are no errors.")
+	case 1:
+		fmt.Printf("Error: %s\n", msg.Errors[0])
+	default:
+		fmt.Printf("There are %d errors:\n", errorsCount)
+		for i, err := range msg.Errors {
+			fmt.Printf("%d) %s\n", i, err)
+		}
+	}
+	return nil
+}
