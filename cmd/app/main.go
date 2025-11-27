e0010e8f11e0ed80ba50f819a139f9099ee29a2a
Ilya Kutilin <ilyakutilin@yandex.ru>
2025-11-27 08:48:49 +0300

    Checking permissions after ensuring workdir

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -55,16 +55,6 @@ func main() {
 		}
 	}()
 
-	ctx := context.Background()
-
-	if !app.debug {
-		if err := utils.CheckPermissions(
-			ctx, app.xrayServiceName, app.workdir, nil,
-		); err != nil {
-			app.logger.Error.Fatal(err)
-		}
-	}
-
 	// Check if the workdir exists, if not create it
 	if err := utils.EnsureDir(cfg.Workdir); err != nil {
 		app.sendMsg(
@@ -75,6 +65,16 @@ func main() {
 				"and nothing else was done.", cfg.Workdir, err),
 		)
 		app.logger.Error.Fatalf("Error creating workdir: %v", err)
+	}
+
+	ctx := context.Background()
+
+	if !app.debug {
+		if err := utils.CheckPermissions(
+			ctx, app.xrayServiceName, app.workdir, nil,
+		); err != nil {
+			app.logger.Error.Fatal(err)
+		}
 	}
 
 	if err := app.updateMultipleFiles(ctx, cfg.Repos, NewFile); err != nil {
